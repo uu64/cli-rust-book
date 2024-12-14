bf952a153dbe717133aca64a2d0af69d739cf101
uu64 <yusuke.t.0808@gmail.com>
2024-12-14 11:19:49 +0900

    add: echo

---
+++ echor/src/main.rs
@@ -0,0 +1,29 @@
+use clap::{Arg, ArgAction, Command};
+
+fn main() {
+    let matches = Command::new("echor")
+    .version("0.1.0")
+    .author("uu64")
+    .about("Rust echo")
+    .arg(
+        Arg::new("text")
+        .value_name("TEXT")
+        .help("Input text")
+        .required(true)
+        .num_args(1..)
+    )
+    .arg(
+        Arg::new("omit_newline")
+        .short('n')
+        .action(ArgAction::SetTrue)
+        .help("Do not print newline")
+    )
+    .get_matches();
+
+    let text: Vec<String> = 
+        matches.get_many("text").unwrap().cloned().collect();
+    
+    let omit_newline = matches.get_flag("omit_newline");
+
+    print!("{}{}", text.join(" "), if omit_newline {""} else {"\n"});
+}
