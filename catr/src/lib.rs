11198712f1c9ecc4f0a9bf238e345044042f2c0c
uu64 <yusuke.t.0808@gmail.com>
2024-12-29 10:55:50 +0900

    update: catr

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, fs::File, io::{self, BufRead, BufReader}};
 
 use clap::{Arg, ArgAction, Command};
 
@@ -17,7 +17,7 @@ pub fn get_args() -> MyResult<Config> {
     .author("uu64")
     .about("Rust cat")
     .arg(
-        Arg::new("file")
+        Arg::new("files")
         .value_name("FILE")
         .default_value("-")
         .help("Input file(s)")
@@ -36,24 +36,52 @@ pub fn get_args() -> MyResult<Config> {
         .short('b')
         .action(ArgAction::SetTrue)
         .help("Number the non-blank output lines, starting at 1.")
+        .conflicts_with("number_lines")
     )
     .get_matches();
 
-    let number_lines = matches.get_flag("number_lines");
-    let number_nonblank_lines = matches.get_flag("number_nonblank_lines");
-
-    if number_lines && number_nonblank_lines {
-        Err("error: The argument '--number-nonblank' cannot be used with '--number'")?
-    }
-
     Ok(Config {
-        files: matches.get_many("file").unwrap().cloned().collect(),
-        number_lines: number_lines,
-        number_nonblank_lines: number_nonblank_lines,
+        files: matches.get_many("files").unwrap().cloned().collect(),
+        number_lines: matches.get_flag("number_lines"),
+        number_nonblank_lines: matches.get_flag("number_nonblank_lines"),
     })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open{}: {}", filename, err),
+            Ok(reader) => print(reader, config.number_lines, config.number_nonblank_lines),
+        }
+    }
     Ok(())
+}
+
+pub fn print(reader: Box<dyn BufRead>, number_lines: bool, number_nonblank_lines: bool) -> () {
+    let mut line_number = 1;
+    for line in reader.lines() {
+        match line {
+            Err(err) => panic!("failed to read line: {:?}", err),
+            Ok(s) => {
+                if number_nonblank_lines && s.is_empty() {
+                    println!();
+                    continue;
+                }
+
+                let mut prefix = String::from("");
+                if number_lines || (number_nonblank_lines && !s.is_empty()) {
+                    prefix.push_str(format!("{line_number:>6}\t").as_str());
+                }
+                println!("{prefix}{s}");
+                line_number += 1;
+            },
+        }
+    }
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
