3b087739c4635a7f7d03bf3681867a4f39412afc
uu64 <yusuke.t.0808@gmail.com>
2025-03-15 14:05:28 +0900

    feat: add count function for wcr

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs::File, io::{self, BufRead, BufReader, Read}};
+use std::{error::Error, fs::File, io::{self, BufRead, BufReader}};
 
 use clap::Parser;
 
@@ -16,6 +16,64 @@ pub struct Config {
     bytes: bool,
     #[arg(short='m', long, default_value_t=false, conflicts_with("bytes"))]
     chars: bool,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct FileInfo {
+    num_lines: usize,
+    num_words: usize,
+    num_bytes: usize,
+    num_chars: usize,
+}
+
+pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+    let mut num_lines = 0;
+    let mut num_words = 0;
+    let mut num_bytes = 0;
+    let mut num_chars = 0;
+
+    let mut buf = String::new();
+    while let Ok(size) = file.read_line(&mut buf) {
+        if size == 0 {
+            // reached EOF
+            break;
+        }
+
+        num_lines += 1;
+        num_words += buf.split_ascii_whitespace().count();
+        num_bytes += size;
+        num_chars += buf.bytes().len();
+
+        buf.clear();
+    }
+    Ok(FileInfo {
+        num_lines,
+        num_words,
+        num_bytes,
+        num_chars,
+    })
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{count, FileInfo};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_count() {
+        let text = "I don't want the world. I just want your half.\r\n";
+        let info = count(Cursor::new(text));
+
+        assert!(info.is_ok());
+        let expected = FileInfo {
+            num_lines: 1,
+            num_words: 10,
+            num_chars: 48,
+            num_bytes: 48,
+        };
+        assert_eq!(info.unwrap(), expected);
+    }
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -39,7 +97,10 @@ pub fn run(config: Config) -> MyResult<()> {
     for filename in &config.files {
         match open(filename) {
             Err(err) => eprintln!("{}: {}", filename, err),
-            Ok(_) => println!("Opened: {}", filename),
+            Ok(_) =>  {
+                println!("Opened: {}", filename);
+                _ = count(open(filename).unwrap());
+            },
         }
     }
     Ok(())
