876f2a2887776e919505149672407ca2b8aee4cd
uu64 <yusuke.t.0808@gmail.com>
2025-11-09 11:46:15 +0900

    fix

--- cutr/src/lib.rs
+++ cutr/src/lib.rs
@@ -1,6 +1,10 @@
 use clap::Args;
 use clap::Parser;
 use regex::bytes::Regex;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
 use std::str::FromStr;
 use std::sync::LazyLock;
 use std::{error::Error, ops::Range};
@@ -13,33 +17,28 @@ static RE_POS_RANGE: LazyLock<Regex> = LazyLock::new(|| {
 });
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-struct PosRange(Vec<Range<usize>>);
+struct PositionList(Range<usize>);
 
-impl FromStr for PosRange {
+impl FromStr for PositionList {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut ranges: Vec<Range<usize>> = vec![];
-
-        for s in s.split(',') {
-            if !RE_POS_RANGE.is_match(s.as_bytes()) {
-                return Err(format!("illegal list value: \"{}\"", s));
-            }
-
-            if let Some((a, b)) = s.split_once('-') {
-                let start: usize = a.parse().map_err(|_| format!("invalid start: {}", a))?;
-
-                let end: usize = b.parse().map_err(|_| format!("invalid end: {}", b))?;
-                if end <= start {
-                    return Err(format!("First number in range ({}) must be lower than second number ({})", start, end));
-                }
-                ranges.push(start-1..end);
-            } else {
-                let n: usize = s.parse().map_err(|_| format!("invalid number: {}", s))?;
-                ranges.push(n-1..n);
-            }
+        if !RE_POS_RANGE.is_match(s.as_bytes()) {
+            return Err(format!("illegal list value: \"{}\"", s));
         }
-        Ok(PosRange(ranges))
+
+        if let Some((a, b)) = s.split_once('-') {
+            let start: usize = a.parse().map_err(|_| format!("invalid start: {}", a))?;
+
+            let end: usize = b.parse().map_err(|_| format!("invalid end: {}", b))?;
+            if end <= start {
+                return Err(format!("First number in range ({}) must be lower than second number ({})", start, end));
+            }
+            Ok(PositionList(start-1..end))
+        } else {
+            let n: usize = s.parse().map_err(|_| format!("invalid number: {}", s))?;
+            Ok(PositionList(n-1..n))
+        }
     }
 }
 
@@ -54,12 +53,12 @@ fn parse_delimiter(s: &str) -> Result<u8, String> {
 #[derive(Args, Debug)]
 #[group(required = true, multiple = false)]
 struct Extract {
-    #[arg(short, long, help="Selected fields")]
-    fields: Vec<PosRange>,
-    #[arg(short, long, help="Selected bytes")]
-    bytes: Vec<PosRange>,
-    #[arg(short, long, help="Selected characters")]
-    chars: Vec<PosRange>,
+    #[arg(short, long, value_delimiter=',', help="Selected fields")]
+    fields: Vec<PositionList>,
+    #[arg(short, long, value_delimiter=',', help="Selected bytes")]
+    bytes: Vec<PositionList>,
+    #[arg(short, long, value_delimiter=',', help="Selected characters")]
+    chars: Vec<PositionList>,
 }
 
 #[derive(Debug, Parser)]
@@ -79,21 +78,45 @@ pub fn get_args() -> MyResult<Config> {
 
 pub fn run(config:Config) -> MyResult<()> {
     println!("{:#?}", &config);
-    // for file in config.files {
-    //     match &file[..] {
-    //         "-" => process_file(&mut std::io::stdin(), file.as_str())?,
-    //         _ => {
-    //             let f = std::fs::file::open(file)?;
-    //             process_file(&mut std::io::bufreader::new(f), file.as_str())?
+    // for filename in &config.files {
+    //     match open(filename) {
+    //         Err(err) => eprintln!("{}: {}", filename, err),
+    //         Ok(_) => {
+    //             println!("Opened {}", filename)
+    //             // extract_chars("hoge", config.extract.chars);
     //         }
     //     }
     // }
     Ok(())
 }
 
-// fn process_file(f: &mut dyn std::io::Read, filename: &str) -> MyResult<()> {
-//     Ok(())
-// }
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?)))
+    }
+}
+
+fn extract_chars(line: &str, char_ops: &[Range<usize>]) -> String {
+    // let mut s = String::from("");
+    for range in char_ops {
+        println!("{}", range.start);
+        println!("{}", range.end);
+        // for {
+
+        // }
+
+        // let chars = line.char_indices();
+        // let start = chars.nth(range.start).0;
+        // let end = chars.nth(range.end).0;
+        // match line.get(start..end) {
+        //     Some(sub) => s += sub,
+        //     None => println!("out of range"),
+        // }
+    }
+    String::from("")
+}
+
 
 #[cfg(test)]
 mod unit_tests {
@@ -102,28 +125,28 @@ mod unit_tests {
     #[test]
     fn test_parse_pos() {
         // The empty string is an error
-        assert!(PosRange::from_str("").is_err());
+        assert!(PositionList::from_str("").is_err());
 
         // Zero is an error
-        let res = PosRange::from_str("0");
+        let res = PositionList::from_str("0");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "0""#);
-        let res = PosRange::from_str("0-1");
+        let res = PositionList::from_str("0-1");
         assert!(res.is_err());
         // assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "0""#);
         assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "0-1""#);
 
         // A leading "+" is an error
-        let res = PosRange::from_str("+1");
+        let res = PositionList::from_str("+1");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "+1""#,);
-        let res = PosRange::from_str("+1-2");
+        let res = PositionList::from_str("+1-2");
         assert!(res.is_err());
         assert_eq!(
             res.unwrap_err().to_string(),
             r#"illegal list value: "+1-2""#,
         );
-        let res = PosRange::from_str("1-+2");
+        let res = PositionList::from_str("1-+2");
         assert!(res.is_err());
         assert_eq!(
             res.unwrap_err().to_string(),
@@ -131,41 +154,41 @@ mod unit_tests {
         );
 
         // Any non-number is an error
-        let res = PosRange::from_str("a");
+        let res = PositionList::from_str("a");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "a""#);
-        let res = PosRange::from_str("1,a");
-        assert!(res.is_err());
-        assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "a""#);
-        let res = PosRange::from_str("1-a");
+        // let res = PositionList::from_str("1,a");
+        // assert!(res.is_err());
+        // assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "a""#);
+        let res = PositionList::from_str("1-a");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "1-a""#,);
-        let res = PosRange::from_str("a-1");
+        let res = PositionList::from_str("a-1");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "a-1""#,);
 
         // Wonky ranges
-        let res = PosRange::from_str("-");
+        let res = PositionList::from_str("-");
         assert!(res.is_err());
-        let res = PosRange::from_str(",");
+        let res = PositionList::from_str(",");
         assert!(res.is_err());
-        let res = PosRange::from_str("1,");
+        let res = PositionList::from_str("1,");
         assert!(res.is_err());
-        let res = PosRange::from_str("1-");
+        let res = PositionList::from_str("1-");
         assert!(res.is_err());
-        let res = PosRange::from_str("1-1-1");
+        let res = PositionList::from_str("1-1-1");
         assert!(res.is_err());
-        let res = PosRange::from_str("1-1-a");
+        let res = PositionList::from_str("1-1-a");
         assert!(res.is_err());
 
         // First number must be less than second
-        let res = PosRange::from_str("1-1");
+        let res = PositionList::from_str("1-1");
         assert!(res.is_err());
         assert_eq!(
             res.unwrap_err(),
             "First number in range (1) must be lower than second number (1)"
         );
-        let res = PosRange::from_str("2-1");
+        let res = PositionList::from_str("2-1");
         assert!(res.is_err());
         assert_eq!(
             res.unwrap_err(),
@@ -173,30 +196,40 @@ mod unit_tests {
         );
 
         // All the following are acceptable
-        let res = PosRange::from_str("1");
+        let res = PositionList::from_str("1");
         assert!(res.is_ok());
-        assert_eq!(res.unwrap().0, vec![0..1]);
-        let res = PosRange::from_str("01");
+        assert_eq!(res.unwrap().0, 0..1);
+        let res = PositionList::from_str("01");
         assert!(res.is_ok());
-        assert_eq!(res.unwrap().0, vec![0..1]);
-        let res = PosRange::from_str("1,3");
+        assert_eq!(res.unwrap().0, 0..1);
+        // let res = PositionList::from_str("1,3");
+        // assert!(res.is_ok());
+        // assert_eq!(res.unwrap().0, vec![0..1, 2..3]);
+        // let res = PositionList::from_str("001,0003");
+        // assert!(res.is_ok());
+        // assert_eq!(res.unwrap().0, vec![0..1, 2..3]);
+        let res = PositionList::from_str("1-3");
         assert!(res.is_ok());
-        assert_eq!(res.unwrap().0, vec![0..1, 2..3]);
-        let res = PosRange::from_str("001,0003");
+        assert_eq!(res.unwrap().0, 0..3);
+        let res = PositionList::from_str("0001-03");
         assert!(res.is_ok());
-        assert_eq!(res.unwrap().0, vec![0..1, 2..3]);
-        let res = PosRange::from_str("1-3");
-        assert!(res.is_ok());
-        assert_eq!(res.unwrap().0, vec![0..3]);
-        let res = PosRange::from_str("0001-03");
-        assert!(res.is_ok());
-        assert_eq!(res.unwrap().0, vec![0..3]);
-        let res = PosRange::from_str("1,7,3-5");
-        assert!(res.is_ok());
-        assert_eq!(res.unwrap().0, vec![0..1, 6..7, 2..5]);
-        let res = PosRange::from_str("15,19-20");
-        assert!(res.is_ok());
-        assert_eq!(res.unwrap().0, vec![14..15, 18..20]);
+        assert_eq!(res.unwrap().0, 0..3);
+        // let res = PositionList::from_str("1,7,3-5");
+        // assert!(res.is_ok());
+        // assert_eq!(res.unwrap().0, vec![0..1, 6..7, 2..5]);
+        // let res = PositionList::from_str("15,19-20");
+        // assert!(res.is_ok());
+        // assert_eq!(res.unwrap().0, vec![14..15, 18..20]);
     }
+
+    // #[test]
+    // fn test_extract_chars() {
+    //     assert_eq!(extract_chars("", &[0..1]), "".to_string());
+    //     assert_eq!(extract_chars("ábc", &[0..1]), "á".to_string());
+    //     // assert_eq!(extract_chars("ábc", &[0..1, 2..3]), "ác".to_string());
+    //     // assert_eq!(extract_chars("ábc", &[0..3]), "ábc".to_string());
+    //     // assert_eq!(extract_chars("ábc", &[2..3, 1..2]), "cb".to_string());
+    //     // assert_eq!(extract_chars("ábc", &[0..1, 1..2, 4..5]), "áb".to_string());
+    // }
 }
 
