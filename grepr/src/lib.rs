a658f9205a7bf3c3372ef6e28809066a9d712e54
uu64 <yusuke.t.0808@gmail.com>
2026-02-23 10:58:46 +0900

    [grepr] feat: config

---
+++ grepr/src/lib.rs
@@ -0,0 +1,33 @@
+use std::error::Error;
+
+use clap::Parser;
+use regex::Regex;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+pub struct Config {
+    #[arg(value_name="PATTERN", help="Search pattern")]
+    pattern: Regex,
+    #[arg(value_name="FILE", help="Input file(s)", default_value="-")]
+    files: Vec<String>,
+    #[arg(short, long, help="Recursive search")]
+    recursive: bool,
+    #[arg(short, long, help="Count occurrences")]
+    count: bool,
+    #[arg(short='v', long, help="Invert match")]
+    invert_match: bool,
+    #[arg(short, long, help="Case-insensitive")]
+    insensitive: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let config = Config::parse();
+    Ok(config)
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
