a628a9be51cb368097b6e7cd491f3051e3e4e106
uu64 <yusuke.t.0808@gmail.com>
2026-02-28 11:36:05 +0900

    test: dies

--- grepr/src/lib.rs
+++ grepr/src/lib.rs
@@ -1,24 +1,24 @@
 use std::error::Error;
 
 use clap::Parser;
-use regex::Regex;
+use regex::{Regex, RegexBuilder};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 pub struct Config {
-    #[arg(value_name="PATTERN", help="Search pattern")]
-    pattern: Regex,
-    #[arg(value_name="FILE", help="Input file(s)", default_value="-")]
+    #[arg(value_name = "PATTERN", help = "Search pattern")]
+    pattern: String,
+    #[arg(value_name = "FILE", help = "Input file(s)", default_value = "-")]
     files: Vec<String>,
-    #[arg(short, long, help="Recursive search")]
+    #[arg(short, long, help = "Recursive search")]
     recursive: bool,
-    #[arg(short, long, help="Count occurrences")]
+    #[arg(short, long, help = "Count occurrences")]
     count: bool,
-    #[arg(short='v', long, help="Invert match")]
+    #[arg(short = 'v', long, help = "Invert match")]
     invert_match: bool,
-    #[arg(short, long, help="Case-insensitive")]
+    #[arg(short, long, help = "Case-insensitive")]
     insensitive: bool,
 }
 
@@ -29,5 +29,9 @@ pub fn get_args() -> MyResult<Config> {
 
 pub fn run(config: Config) -> MyResult<()> {
     println!("{:#?}", config);
+    let _ = RegexBuilder::new(&config.pattern)
+        .case_insensitive(config.insensitive)
+        .build()
+        .map_err(|e| format!("Invalid pattern \"{}\": {}", config.pattern, e))?;
     Ok(())
 }
